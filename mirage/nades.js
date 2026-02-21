8aa71d69d027f35347f8da05e0a14f09e3e16584
Yin Jun <1171276297yin@gmail.com>
2026-02-21 09:20:09 +0100

    mirage done

--- mirage/nades.js
+++ mirage/nades.js
@@ -1,4 +1,68 @@
 // mirage/nades.js
 const nadesData = [
+    {
+    id: "n-471838",
+    type: "smoke",
+    name: "CT_smoke",
+    startX: "94.01", startY: "50.55",
+    endX: "42.43", endY: "90.86",
+    throwType: "Jump Throw",
+},
+{
+    id: "n-861456",
+    type: "smoke",
+    name: "B_short_smoke",
+    startX: "60.13", startY: "3.70",
+    endX: "37.66", endY: "21.87",
+    throwType: "Jump Throw"
+},
+{
+    id: "n-050894",
+    type: "smoke",
+    name: "market_window_smoke",
+    startX: "60.13", startY: "3.70",
+    endX: "19.61", endY: "33.02",
+    throwType: "Jump Throw"
+},
+{
+    id: "n-278645",
+    type: "smoke",
+    name: "market_smoke",
+    startX: "60.13", startY: "3.70",
+    endX: "9.88", endY: "33.00",
+    throwType: "Jump Throw"
+},
+{
+    id: "n-540236",
+    type: "smoke",
+    name: "staris_smoke",
+    startX: "91.56", startY: "59.61",
+    endX: "53.12", endY: "69.00",
+    throwType: "Standing"
+},
+{
+    id: "n-624933",
+    type: "smoke",
+    name: "jungle_smoke",
+    startX: "83.05", startY: "66.17",
+    endX: "48.27", endY: "70.34",
+    throwType: "Standing"
+},
+{
+    id: "n-789600",
+    type: "smoke",
+    name: "connector_smoke",
+    startX: "90.76", startY: "10.08",
+    endX: "48.57", endY: "49.49",
+    throwType: "Jump Throw"
+},
+{
+    id: "n-954759",
+    type: "smoke",
+    name: "B_short_smoke_2",
+    startX: "97.78", startY: "26.57",
+    endX: "45.05", endY: "32.41",
+    throwType: "Jump Throw"
+},
     
 ];
